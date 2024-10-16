6094cebd0cb0ec4d4d368b520a1a47843eb6331f
vhvicente99@gmail.com <vhvicente99@gmail.com>
2024-10-16 19:46:49 -0300

    Atualizações nos arquivos Letreiro e Relogio

--- src/Letreiro.js
+++ src/Letreiro.js
@@ -1,11 +1,23 @@
+import React, {useState} from 'react';
 
-function Letreiro() {
+const text = "Venha estudar na Fatec"
+
+function Letreiro() 
+{
+  const [count, setCount] = useState(0);
+
+  if (count <= text.length) setTimeout(()=>setCount(count + 1),300)
+  else setCount(0);
+
   return (
-    <>
-      <div>
-        <h1>Meu Letreiro</h1>
-      </div>
-    </>
+
+  <div>
+
+    <h1 style={{ color: 'red' }}>Meu Letreiro</h1>
+    <h1>{text.slice(0,count)}</h1>
+
+  </div>
+  
   );
 }
 
