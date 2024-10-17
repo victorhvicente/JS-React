eb85653382bf3ade48e8cf38d0d8dc6634d56337
Victor Hugo Vicente <157495513+victorhvicente@users.noreply.github.com>
2024-10-17 10:45:34 -0300

    Update Atv01.js

--- src/Atv01.js
+++ src/Atv01.js
@@ -5,8 +5,10 @@ import Letreiro from "./Letreiro";
 function Atv01() {
     return(
 
-        <>
+         <>
             <h1>Atividade 01</h1>
+            <Relogio/>
+            <Letreiro/>
             <Link to="/">Voltar</Link>
         </>
 
