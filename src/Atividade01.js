350718b912904c4e2e2fe87e0688e48c57ece9f6
vhvicente99@gmail.com <vhvicente99@gmail.com>
2024-10-22 13:34:35 -0300

    Resolve merge conflict in Atividade01.js

--- src/Atividade01.js
+++ src/Atividade01.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 import {Link} from "react-router-dom";
 import Relogio from "./Relogio";
 import Letreiro from "./Letreiro";
 
-function Atv01() {
+function Atividade01() {
     return(
 
          <>
@@ -16,24 +15,4 @@ function Atv01() {
     );
 }
 
-export default Atv01;
-=======
-import {Link} from "react-router-dom";
-import Relogio from "./Relogio";
-import Letreiro from "./Letreiro";
-
-function Atv01() {
-    return(
-
-         <>
-            <h1>Atividade 01</h1>
-            <Relogio/>
-            <Letreiro/>
-            <Link to="/">Voltar</Link>
-        </>
-
-    );
-}
-
-export default Atv01;
->>>>>>> 6c10e0e1d7457779598a3677623194481e4c2cea
+export default Atividade01;
