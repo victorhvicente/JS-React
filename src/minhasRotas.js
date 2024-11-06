646f693719df7df5427aa6420f82ed62dd440eed
vhvicente99@gmail.com <vhvicente99@gmail.com>
2024-11-06 10:42:09 -0300

    Atualização dos arquivos

--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -1,18 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./Home";
-import Atividade01 from "./Atividade01";
-import Atividade02 from "./Atividade02";
+import Atividade01 from "./Atividade01/Atividade01";
+import Atividade02 from "./Atividade02/Atividade02";
 
-export default function MinhasRotas(){
+export default function MinhasRotas() {
     return (
 
         <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/Atividade01" element={<Atividade01/>}/>
-            <Route path="/Atividade02" element={<Atividade02/>}/>
-        </Routes>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Atividade01" element={<Atividade01 />} />
+                <Route path="/Atividade02" element={<Atividade02 />} />
+            </Routes>
         </BrowserRouter>
     );
 }
