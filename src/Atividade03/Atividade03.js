8c987e169613caf2e20834ff605c86aec9959fdb
vhvicente99@gmail.com <vhvicente99@gmail.com>
2024-11-06 12:58:04 -0300

    Novos arquivos da atividade 03

---
+++ src/Atividade03/Atividade03.js
@@ -0,0 +1,18 @@
+import {Link} from "react-router-dom";
+import Gallery from "./Gallery.js";
+import { Profile } from './Gallery.js';
+import Avatar from './Avatar.js';
+
+
+export default function Atividade03() {
+    
+    return (
+        <>
+            <h1>Atividade03</h1>
+            <br/>
+            <Profile />
+            <br/>
+            <Avatar />
+        </>
+    );
+}
