17eadd8ea71ca6719ed2a39a351c9a325206be73
vhvicente99@gmail.com <vhvicente99@gmail.com>
2024-11-15 10:58:49 -0300

    adicionado componente Toolbar

--- src/Atividade04/Atividade04.js
+++ src/Atividade04/Atividade04.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import Toolbar from "./Toolbar";
 
 function Atividade04() {
     return(
 
          <>
-            <h1>Atividade 04</h1>
-       
-        </>
+            <h1>Atividade 04</h1><br/><br/>
+            <Toolbar />
+        </> 
 
     );
 }
