f32c757bed551a03559ac3ad6030746daf18977b
vhvicente99@gmail.com <vhvicente99@gmail.com>
2024-11-15 11:46:13 -0300

    Componente Gallery

--- src/Atividade04/Atividade04.js
+++ src/Atividade04/Atividade04.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import Toolbar from "./Toolbar";
+import Gallery from "./Gallery";
 
 function Atividade04() {
     return(
 
          <>
             <h1>Atividade 04</h1><br/><br/>
-            <Toolbar />
+            <Toolbar /><br/><br/>
+            <Gallery />
         </> 
 
     );
